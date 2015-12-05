8bd5b3e8fa9e81ecb73a96ff9b1d7ab66934e090
ana3021 <anadelgado3007@gmail.com>
2015-12-04 19:31:07 -0500

    Modificación mensaje bienvenida
    Modificación del mensaje de bienvenida del sistema anei.

--- Catación Anei/Formularios/Gestionar personas/Bienvenido.vb
+++ Catación Anei/Formularios/Gestionar personas/Bienvenido.vb
@@ -8,4 +8,8 @@
         'Me.Visible = False
         'frmPerfil.ShowDialog()
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
